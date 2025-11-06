105e17d33e17c54cf5c7eed7770c94954740fc01
Иван Закачурин <vanekcarek43@gmail.com>
2025-11-06 23:52:44 +0500

    Task1

---
+++ Tyuiu.ZakachurinIE.Sprint4.Task1.V9/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.ZakachurinIE.Sprint4.Task1.V9.Lib;
+
+namespace Tyuiu.ZakachurinIE.Sprint4.Task1.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт # 4 | Выполнил: Закачурин И. Е. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (ввод с клавиатуры)                           *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Варинат #09                                                             *");
+            Console.WriteLine("* Выполнил: Закачурин Иван Евгеньевич | ИБКСб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* подсчитать произведение четных элементов массива                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int len;
+            Console.Write("Введите колво элементов в массиве ");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write("Введите значение "+i+" элемента массива ");
+                array[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0;i < len; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(array));
+        }
+    }
+}
